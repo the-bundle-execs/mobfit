ab3dadd015beaf3c252314daff5070acf8f392eb
Mary Magee <mmagee322@gmail.com>
2019-04-22 14:33:16 -0700

    Events component created

--- app/javascript/components/pages/Events.js
+++ app/javascript/components/pages/Events.js
@@ -1,10 +1,46 @@
 import React from "react"
 import PropTypes from "prop-types"
+import { allEvents } from "../api"
 
 class Events extends React.Component {
+  constructor(props){
+     super(props)
+     this.state = {
+       error: null,
+       events: [],
+     }
+     this.getEvents()
+   }
+
+
+   getEvents = ()=>{
+     allEvents()
+     .then((events)=>{
+       console.log('events', events);
+       this.setState({ events })
+     })
+     .catch((error) => {
+       this.setState({
+         error
+       })
+     })
+   }
+
+
   render () {
     return (
       <React.Fragment>
+      <div>
+            <h1>Events</h1>
+            {this.state.events.map((evt, index) =>{
+              return(
+              <ul>
+              <li key={index}>
+              {evt.event_name}<br></br>{evt.location_name}<br></br>{evt.activity}<br></br>{evt.level}
+              </li>
+              </ul>
+            )})}
+            </div>
       </React.Fragment>
     );
   }
