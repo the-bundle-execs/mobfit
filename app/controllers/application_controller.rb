cca9e3e7346b2b08617f25ccc97fd6f4d861fc03
jessigu <jessigu@gmail.com>
2019-04-23 16:57:49 -0700

    fixed sign up trainer toggle

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,8 +1,5 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery prepend: true
-#tried 2 below for create event 422
-  # protect_from_forgery with: :null_session
-  # protect_from_forgery unless: -> { request.format.json? }
   before_action :configure_permitted_parameters, if: :devise_controller?
 
   def after_sign_in_path_for(resource)
@@ -14,6 +11,6 @@ class ApplicationController < ActionController::Base
   end
 
   def configure_permitted_parameters
-    devise_parameter_sanitizer.permit(:sign_up, keys: [:email, :username])
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:email, :username, :is_trainer])
   end
 end
