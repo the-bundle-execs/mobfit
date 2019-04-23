62a1aeb531ea2fbcde77e5edfb062bcfc2faa745
EC2 Default User <ec2-user@ip-172-31-40-65.us-east-2.compute.internal>
2019-04-23 23:49:07 +0000

    merge fine. unauthenticated up to date

--- app/javascript/components/UnauthenticatedApp.js
+++ app/javascript/components/UnauthenticatedApp.js
@@ -1,7 +1,11 @@
 import React from "react"
 import PropTypes from "prop-types"
 import Events from './pages/Events'
-import { Button, Navbar, Nav } from 'react-bootstrap'
+
+import { Button, Navbar, Nav, Jumbotron, Carousel  } from 'react-bootstrap'
+import Logo from './images/mobfit.png'
+import Image from './images/beachDay.jpg'
+
 import { allEvents} from './api'
 
 class UnauthenticatedApp extends React.Component {
@@ -31,24 +35,64 @@ class UnauthenticatedApp extends React.Component {
     let { events } = this.state
     return (
       <React.Fragment>
-          <Navbar variant="navbar navbar-expand-lg navbar-dark bg-primary">
+      <Jumbotron href="#home" id="landingpage" fluid className="text-xs-center">
+          <Navbar variant="navbar navbar-expand-lg navbar-dark bg-primary" className="nav-bar">
+          <Navbar.Brand img src={Logo} href="#home">
+          <img src={Logo}className="logo" alt="Logo" />
+          </Navbar.Brand>
+
           <Navbar.Brand href="#home">MobFit</Navbar.Brand>
           <Navbar.Toggle aria-controls="basic-navbar-nav" />
           <Navbar.Collapse id="basic-navbar-nav">
           <Nav variant="mr-auto">
             <Nav.Link href="#home">Home</Nav.Link>
-            <Nav.Link href="#link">Link</Nav.Link>
+            <Nav.Link href="#link">About Us</Nav.Link>
+            <Nav.Link href="/users/sign_in">Login</Nav.Link><br/>
+           <Nav.Link href="/users/sign_up">Sign up</Nav.Link>
+
           </Nav>
           </Navbar.Collapse>
           </Navbar>;
 
-          <Button type="button" href="/users/sign_in" variant="btn btn-outline-primary">Primary</Button>
-
         <h1>Welcome to MobFit!</h1>
-        <h4>Please Login to Continue</h4>
-        <a href="/users/sign_in">Login</a><br/>
-        <a href="/users/sign_up">Sign up</a>
-        < Events events = {events}/>
+       </Jumbotron>
+
+       <Carousel>
+  <Carousel.Item>
+    <img className= "beach-day" src={Image} alt="First Slide" />
+
+    <Carousel.Caption>
+      <h3>First slide label</h3>
+      <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
+    </Carousel.Caption>
+  </Carousel.Item>
+  <Carousel.Item>
+    <img
+      className="d-block w-100"
+      src=""
+      alt="Third slide"
+    />
+
+    <Carousel.Caption>
+      <h3>Second slide label</h3>
+      <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+    </Carousel.Caption>
+  </Carousel.Item>
+  <Carousel.Item>
+
+    <img
+      className="d-block w-100"
+      src="holder.js/800x400?text=Third slide&bg=20232a"
+      alt="Third slide"
+    />
+
+    <Carousel.Caption>
+      <h3>Third slide label</h3>
+      <p>Praesent commodo cursus magna, vel scelerisque nisl consectetur.</p>
+    </Carousel.Caption>
+  </Carousel.Item>
+</Carousel>;
+
       </React.Fragment>
     );
   }
