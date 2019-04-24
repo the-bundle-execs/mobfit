434d38f7e41f9ac1c843d6020cc288ed1479b4aa
jessigu <jessigu@gmail.com>
2019-04-24 07:41:37 -0700

    modal size, columns, button save and close

--- app/javascript/components/pages/NewEvent.js
+++ app/javascript/components/pages/NewEvent.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react'
-import { Form, Modal, Button } from 'react-bootstrap'
+import { Form, Modal, Button, Container, Row, Col } from 'react-bootstrap'
 
 class NewEvent extends Component {
   constructor(props){
@@ -31,6 +31,7 @@ class NewEvent extends Component {
   submitEvent = () => {
     const { form } = this.state
     this.props.addEvent(form)
+    this.props.onHide()
   }
 
     render() {
@@ -38,7 +39,7 @@ class NewEvent extends Component {
       const { user, show } = this.props
       return (
           <React.Fragment>
-            <Modal show={show}>
+            <Modal show={show}  size="lg">
               <Modal.Header>
                 <div>
                 <Button
@@ -46,115 +47,148 @@ class NewEvent extends Component {
                   variant="btn btn-sm btn-outline-success"
                   onClick={this.props.onHide}>Close
                 </Button>
-                  <Modal.Title style={{float: "left", width: "400px"}}>Create New Event for {user.username}</Modal.Title>
+                  <Modal.Title style={{float: "left", width: "760px"}}>Create New Event for {user.username}</Modal.Title>
                 </div>
               </Modal.Header>
 
               <Modal.Body>
+                <Container>
                 <Form>
-                  <Form.Control
-                      style={{width: '200px'}}
-                      type="text"
-                      name="trainer_name"
-                      onChange={this.handleChange}
-                      value={this.props.user.username}
-                  />
-                  <Form.Label>Trainer Name</Form.Label>
-                  <Form.Control
-                      style={{width: '200px'}}
-                      type="text"
-                      name="event_name"
-                      onChange={this.handleChange}
-                      value={form.event_name}
-                  />
-                  <Form.Label id="event_name">Event Name</Form.Label>
-                  <Form.Control
+                  <Row className="show-grid">
+                     <Col xs={6} md={4}>
+                       <Form.Label>Trainer Name:</Form.Label>
+                       <Form.Control
+                        style={{width: '200px'}}
+                        type="text"
+                        name="trainer_name"
+                        onChange={this.handleChange}
+                        value={this.props.user.username}
+                       />
+                     </Col>
+                     <Col xs={6} md={4}>
+                       <Form.Label id="event_name">Event Name:</Form.Label>
+                       <Form.Control
+                        style={{width: '200px'}}
+                        type="text"
+                        name="event_name"
+                        onChange={this.handleChange}
+                        value={form.event_name}
+                       />
+                     </Col>
+                     <Col xs={6} md={4}>
+                     <Form.Label id="date">Event Date:</Form.Label>
+                     <Form.Control
                       style={{width: '200px'}}
                       type="date"
                       name="date"
                       onChange={this.handleChange}
                       value={form.date}
-                  />
-                  <Form.Label id="date">Event Date</Form.Label>
-                  <Form.Control
-                      style={{width: '200px'}}
-                      type="time"
-                      name="time"
-                      onChange={this.handleChange}
-                      value={form.time}
-                  />
-                  <Form.Label id="time">Event Time</Form.Label>
-                  <Form.Control
-                      style={{width: '200px'}}
-                      type="text"
-                      name="duration"
-                      onChange={this.handleChange}
-                      value={form.duration}
-                  />
-                  <Form.Label id="duration">Event Duration</Form.Label>
-                  <Form.Control
-                      style={{width: '200px'}}
-                      type="text"
-                      name="location_name"
-                      onChange={this.handleChange}
-                      value={form.location_name}
-                  />
-                  <Form.Label id="location_name">Event Location</Form.Label>
-                  <Form.Control
-                      style={{width: '200px'}}
-                      type="text"
-                      name="activity"
-                      onChange={this.handleChange}
-                      value={form.activity}
-                  />
-                  <Form.Label id="activity">Event Activity</Form.Label>
-                  <Form.Control
-                      style={{width: '200px'}}
-                      type="text"
-                      name="level"
-                      onChange={this.handleChange}
-                      value={form.level}
-                  />
-                  <Form.Label id="level">Recommended Fitness Level</Form.Label>
-                  <Form.Control
-                      style={{width: '200px'}}
-                      type="number"
-                      name="max_enrollment"
-                      onChange={this.handleChange}
-                      value={form.max_enrollment}
-                  />
-                  <Form.Label id="max_enrollment">Maximum Number of Attendees</Form.Label>
-                  <Form.Control
-                      style={{width: '200px'}}
-                      type="text"
-                      name="equipment"
-                      onChange={this.handleChange}
-                      value={form.equipment}
-                  />
-                  <Form.Label id="equipment">Event Equipment</Form.Label>
-                  <Form.Control
-                      style={{width: '200px'}}
-                      type="text"
-                      name="comments"
-                      onChange={this.handleChange}
-                      value={form.comments}
-                  />
-                  <Form.Label id="comments">Additional Comments</Form.Label>
-                  <br/>
-                  <Button
-                    onClick={this.submitEvent}
-                    variant="btn btn-outline-success" id="submit">
-                    Create New Event
-                  </Button>
+                     />
+                     </Col>
+                   </Row><br/>
+                   <Row className="show-grid">
+                     <Col xs={6} md={4}>
+                      <Form.Label id="time">Event Time:</Form.Label>
+                      <Form.Control
+                        style={{width: '200px'}}
+                        type="time"
+                        name="time"
+                        onChange={this.handleChange}
+                        value={form.time}
+                      />
+                     </Col>
+                     <Col xs={6} md={4}>
+                     <Form.Label id="duration">Event Duration:</Form.Label>
+                       <Form.Control
+                        style={{width: '200px'}}
+                        type="text"
+                        name="duration"
+                        onChange={this.handleChange}
+                        value={form.duration}
+                       />
+                    </Col>
+                    <Col xs={6} md={4}>
+                    <Form.Label id="location_name">Event Location:</Form.Label>
+                      <Form.Control
+                        style={{width: '200px'}}
+                        type="text"
+                        name="location_name"
+                        onChange={this.handleChange}
+                        value={form.location_name}
+                      />
+                    </Col>
+                  </Row><br/>
+                  <Row className="show-grid">
+                    <Col xs={6} md={4}>
+                      <Form.Label id="activity">Event Activity:</Form.Label>
+                      <Form.Control
+                        style={{width: '200px'}}
+                        type="text"
+                        name="activity"
+                        onChange={this.handleChange}
+                        value={form.activity}
+                      />
+                    </Col>
+                    <Col xs={6} md={4}>
+                      <Form.Label id="level">Recommended Fitness Level:</Form.Label>
+                      <Form.Control
+                        style={{width: '200px'}}
+                        type="text"
+                        name="level"
+                        onChange={this.handleChange}
+                        value={form.level}
+                      />
+                    </Col>
+                    <Col xs={6} md={4}>
+                      <Form.Label id="max_enrollment">Maximum Number of Attendees:</Form.Label>
+                      <Form.Control
+                        style={{width: '200px'}}
+                        type="number"
+                        name="max_enrollment"
+                        onChange={this.handleChange}
+                        value={form.max_enrollment}
+                      />
+                    </Col>
+                  </Row><br/>
+                  <Row className="show-grid">
+                    <Col xs={6} md={4}>
+                      <Form.Label id="equipment">Event Equipment:</Form.Label>
+                      <Form.Control
+                          style={{width: '200px'}}
+                          type="text"
+                          name="equipment"
+                          onChange={this.handleChange}
+                          value={form.equipment}
+                      />
+                    </Col>
+                    <Col xs={12} md={8}>
+                      <Form.Label id="comments">Additional Comments:</Form.Label>
+                      <Form.Control
+                          style={{width: '460px'}}
+                          type="text"
+                          name="comments"
+                          onChange={this.handleChange}
+                          value={form.comments}
+                      />
+                    </Col>
+                  </Row><br/>
+                  <Row className="show-grid">
+                    <Col xs={6} md={4}>
+                    </Col>
+                    <Col xs={8} md={6}>
+                      <Button
+                        onClick={this.submitEvent}
+                        variant="btn btn-outline-success" id="submit">
+                        Create New Event & Close Modal
+                      </Button>
+                    </Col>
+                    <Col xs={4} md={2}>
+                    </Col>
+                  </Row>
                 </Form>
+                </Container>
               </Modal.Body>
 
-              <Modal.Footer>
-                <Button
-                  variant="btn btn-sm btn-outline-success"
-                  onClick={this.props.onHide}>Close Modal
-                </Button>
-              </Modal.Footer>
             </Modal>
           </React.Fragment>
       );
