849cbb89ebd99c38bca3af6cf866391a55be2798
jessigu <jessigu@gmail.com>
2019-04-24 14:39:11 -0700

    hosted events on trainer page complete

--- app/javascript/components/HostedEvents.js
+++ app/javascript/components/HostedEvents.js
@@ -5,32 +5,42 @@ import { CardDeck, Card, Button } from "react-bootstrap"
 class HostedEvents extends React.Component {
   render () {
     const { events, user } = this.props
+
+    let hostedEvents = events.filter(event => {
+      return user.id === event.trainer_id
+    });
+
+    let noHostedEvents = events.filter(event => {
+      return user.id !== event.trainer_id
+    });
+
     return (
       <React.Fragment>
         <h1>Hosted Events</h1>
+        {hostedEvents.length === 0 &&
+          <h4 key={user.id}>You are not scheduled to host any events.</h4>
+        }
         <CardDeck>
-          {events.map(event => {
+          {hostedEvents.map(event => {
             return(
               <div key={event.id}>
-                {user.id == event.trainer_id &&
-                  <Card className="text-white bg-primary mb-3">
-                    <Card.Body>
-                      <Card.Title>{event.event_name}</Card.Title>
-                      <Card.Text>
-                        Where: {event.location_name}
-                        <br></br>
-                        What: {event.activity}
-                        <br></br>
-                        When:{event.date}
-                        <br></br>
-                        Time: {event.time}
-                      </Card.Text>
-                    </Card.Body>
-                    <Card.Footer>
-                      <Button variant="light">Edit Hosted Event</Button>
-                    </Card.Footer>
-                  </Card>
-                }
+                <Card className="text-white bg-primary mb-3">
+                  <Card.Body>
+                    <Card.Title>{event.event_name}</Card.Title>
+                    <Card.Text>
+                      Where: {event.location_name}
+                      <br></br>
+                      What: {event.activity}
+                      <br></br>
+                      When:{event.date}
+                      <br></br>
+                      Time: {event.time}
+                    </Card.Text>
+                  </Card.Body>
+                  <Card.Footer>
+                    <Button variant="light">Edit Hosted Event</Button>
+                  </Card.Footer>
+                </Card>
               </div>
             )
           })}
