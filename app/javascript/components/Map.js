0a9888b040c8d7a33a68a08d755420dbf06e03b5
jessigu <jessigu@gmail.com>
2019-04-29 13:54:37 -0700

    gmap api key stored in credentials and added ad prop in Map

--- app/javascript/components/Map.js
+++ app/javascript/components/Map.js
@@ -6,7 +6,7 @@ class Map extends React.Component {
     return (
       <div style={{width: '100%', height: '300px'}}>
         <GoogleMap
-          bootstrapURLKeys='AIzaSyAsLfmEY7AvCvFx4nrsQVsQS0R1A9UAG9w'
+          bootstrapURLKeys={this.props.google_maps_api_key}
           center={[32.7096298,-117.1602029]}
           zoom={12}
         >
