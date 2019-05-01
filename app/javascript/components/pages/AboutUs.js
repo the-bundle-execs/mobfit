a37530ff861ed42a95196438221262817406e8d5
JenkinsCode <robertjenkins84@gmail.com>
2019-05-01 09:42:41 -0700

    add-on style changes

--- app/javascript/components/pages/AboutUs.js
+++ app/javascript/components/pages/AboutUs.js
@@ -4,16 +4,19 @@ import ReactDOM from 'react-dom'
  class AboutUs extends React.Component {
    render() {
      return(
-        <div>
-          <h3>About Us</h3>
-          <p>
-          At MobFit, we believe there is a better way to utilize public spaces. A more valuable, less invasive way
-          where trainers and people can come together and break a sweat!
-          We’re obsessively passionate about it, and our mission is to help people achieve it.
-          </p>
-        </div>
-     )
-   }
+       <div>
+         <h3>About Us</h3>
+         <p>
+         At MobFit, we believe there's a better way to utilize public spaces. A more valuable, less invasive way
+         where trainers and people can come together and break a sweat!
+
+
+
+
+         </p>
+       </div>
+    )
+  }
  }
 
-export default AboutUs
+ export default AboutUs
