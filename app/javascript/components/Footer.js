8c47d8911511fd8ae0a1ba20319003dc899fa5cb
jessigu <jessigu@gmail.com>
2019-05-02 10:45:01 -0700

    footer edits

--- app/javascript/components/Footer.js
+++ app/javascript/components/Footer.js
@@ -1,14 +1,18 @@
 import React from 'react'
-import PropTypes from "prop-types"
 import { FaGithub } from 'react-icons/fa'
 
+const GithubStyle = {
+  // verticalAlign: 'middle',
+  fontSize: '27px'
+}
+//link, inline
 class Footer extends React.Component {
   render () {
     return (
       <React.Fragment>
         <footer className="footer">
-          <a href="https://github.com/the-bundle-execs/mobfit"></a>
-          <p className="copyrights">Copyright &copy; The Bundle Execs 2019</p>
+          <a href="https://github.com/the-bundle-execs/mobfit"><label className="copyrights">Copyright &copy; The Bundle Execs 2019 &nbsp;</label></a>
+          < FaGithub style={GithubStyle} />
         </footer>
       </React.Fragment>
     );
